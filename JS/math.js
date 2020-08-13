ea536a93ea657e46cceb758f5d06e45f22391041
JakirteJak <bg.a1977@gmail.com>
2020-08-13 21:51:52 +0200

    0813

--- JS/math.js
+++ JS/math.js
@@ -1,13 +1,6 @@
 function sont_dataclear() { // Törli a szövegmezőket
-    let mezo1 = document.getElementById("Iminput");
-    mezo1.value = "";
-    mezo1 = document.getElementById("Itinput");
-    mezo1.value = "";
-    mezo1 = document.getElementById("Rminput");
-    mezo1.value = "";
-    mezo1 = document.getElementById("Rsinput");
-    mezo1.value = "";
-}
+    document.getElementById("Iminput").value = ""; document.getElementById("Itinput").value = "";
+    document.getElementById("Rminput").value = ""; document.getElementById("Rsinput").value = ""; }
 
 function sont_szamitas() { // Kiszámolja három mezőből a negyediket
     let Im = parseFloat(document.getElementById("Iminput").value); // Ampermérő méréshatára
@@ -23,16 +16,13 @@ function sont_szamitas() { // Kiszámolja három mezőből a negyediket
     if (document.getElementById("Rsinput").value == "") numofempty++;
 
     if (numofempty > 1) { // Ha több mint egy mező üres, hiba
-        alert("Legalább három mezőt ki kell tölteni!"); return;
-    }
+        alert("Legalább három mezőt ki kell tölteni!"); return; }
     if (numofempty == 0) { // Ha minden mező ki van töltve, hiba
-        alert("Egyszerre csak három mezőt kell kitölteni, a negyediket számolom!"); return;
-    }
+        alert("Egyszerre csak három mezőt kell kitölteni, a negyediket számolom!"); return; }
 
-    // Megszámolja, hogy mező érvénytelen
+    // Megszámolja, hány mező érvénytelen, aza NaN (üres, vagy nem számokat tartalmaz)
     let numofnan = 0;
-    ImOK = true; ItOK = true;
-    RmOK = true; RsOK = true;
+    ImOK = true; ItOK = true; RmOK = true; RsOK = true;
     if (Im.toString() == "NaN") { numofnan++; ImOK = false; }
     if (It.toString() == "NaN") { numofnan++; ItOK = false; }
     if (Rm.toString() == "NaN") { numofnan++; RmOK = false; }
@@ -40,27 +30,21 @@ function sont_szamitas() { // Kiszámolja három mezőből a negyediket
     if (numofnan > 1) { // Ha számok helyett más karakterek vannak megadva, hiba.
                         // Legalább egy mezőt hibásnak lát, amelyiket üresen hagyott a felhasználó.
         alert("Hibás értékmegadás!")
-        return;
-    }
+        return; }
 
-    result = parseFloat(0);
     if (!ImOK) { // Ampermérő méréshatára sönt nélkül számítása
         result = It * Rs / (Rm + Rs);
-        document.getElementById("Iminput").value = parseFloat(result).toPrecision(4);
-    }
+        document.getElementById("Iminput").value = parseFloat(result).toPrecision(4); }
 
-    if (!ItOK) { // Szükséges méréshatár a sönttel
+    if (!ItOK) { // Szükséges méréshatár a sönttel számítása
         result = (Im * Rm + Im * Rs) / Rs;
-        document.getElementById("Itinput").value = parseFloat(result).toPrecision(4);
-    }
+        document.getElementById("Itinput").value = parseFloat(result).toPrecision(4); }
 
     if (!RmOK) { // Műszer belső ellenállása számítása
         result = (It - Im) * Rs / Im;
-        document.getElementById("Rminput").value = parseFloat(result).toPrecision(4);
-    }
+        document.getElementById("Rminput").value = parseFloat(result).toPrecision(4); }
 
     if (!RsOK) { // Sönt ellenállás számítása
         result = Im * Rm / (It - Im);
-        document.getElementById("Rsinput").value = parseFloat(result).toPrecision(4);
-    }
+        document.getElementById("Rsinput").value = parseFloat(result).toPrecision(4); }
 }
