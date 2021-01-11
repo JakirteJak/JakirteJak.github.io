77a5968583029a9ea629939ee456db955a324868
JakirteJak <67140731+JakirteJak@users.noreply.github.com>
2021-01-11 23:50:35 +0100

    21.01.11

--- JS/svg01.js
+++ JS/svg01.js
@@ -1,11 +1,15 @@
 let svg_gear01 = document.getElementById("svg_gear01");
 let svg_txt01 = document.getElementById("svg_txt01");
 let txt01 = document.getElementById("txt01");
-let angle = 20;
+let HT = document.getElementById("heart_test");
+document.addEventListener('mousemove', onMouseUpdate, false);
+let p_mouseXY = document.getElementById("p_mouseXY");
+let angle = 20; let mX = 0; let mY = 0;
 
 svg_txt01.setAttribute("transform", "rotate(" + angle + ")");
 let timerMain      = setInterval(timerMainF, 20);
-
+HT.href ="img/backG01.jpg";
+HT.width = "10";
 function timerMainF() {
     angle++;
     if (angle > 359) angle =  0;
@@ -17,4 +21,14 @@ function timerMainF() {
     svg_txt01.setAttribute("transform", "rotate(" + angle + " " + xH + "," + yH + ")");    
 
     svg_gear01.style.strokeDashoffset = angle / 3.6;
+
+
+}
+
+function onMouseUpdate(e) {
+    mX = e.pageX;
+    mY = e.pageY;
+    HT.x = mX;
+    HT.y = mY.toString();
+    p_mouseXY.innerHTML = "mouseX = " + mX + "<br>mouseY = " + mY + "<br>" + HT.x + "<br>" + HT.y;
 }
