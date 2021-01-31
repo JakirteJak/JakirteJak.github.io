84bf801cbc66e5e3b20168395511abb3879bad61
JakirteJak <67140731+JakirteJak@users.noreply.github.com>
2021-01-31 09:22:49 +0100

    21.01.31

--- JS/game_memory01.js
+++ JS/game_memory01.js
@@ -11,21 +11,27 @@ images_back = []; // háttérképek (img)
 cards       = []; // kértyák (div)
 coords      = [];
 
-let timerMain = setInterval(tMain, 10);
-let timerCardMoveToBase = setInterval(tCardMoveToBase, 5);
+let timerCardFlip = setInterval(tCardFlip, 10);
+let timerCardMoveToBase       = setInterval(tCardMoveToBase, 5);
+let timerAllCardsMoveToBase   = setInterval(tAllCardsMoveToBase, 10);
+let timerAllCardsMoveToNewPos = setInterval(tMoveCardsToNewCoords, 5);
 document.addEventListener("mousemove", function (event) { mouseXY(event) }); // Egérmozgás esemény
 
 let baseCardSize = 100; // alap kártyaszélesség pixelben, ehhez viszonyít
 let actCardSize  = 100;
 
-let cardGrow   = false; // átforduláskor a növekedsi, vagy a csökkenése fázisban van
-let cardRotate = false; // az egyik kártya éppen átfordul
+let cardGrow = false; // átforduláskor a növekedési, vagy a csökkenése fázisban van
+let cardFlip = false; // az egyik kártya éppen átfordul
+let cardFlipToSeePic = true;
+
 let cardMoveToBase = false;
 let growNumber = 5;        // átfordulás sebessége, valójában pixelben megadott érték, ennyit csökkent vagy növel
-let cardFront = true;
 let rowCount = 4; let colCount = 6;   // sorok és oszlopok száma
 let flippedOne = -1; flippedTwo = -1; // átfordított kártyák sorszámai, egyszerre max kettő lehet; -1 esetén nincs kártyaszám
 let distX1, distY1, distX2, distY2;
+let cardDockedCount = 0;
+let cardsDocking = false;
+let cardsPositioning = false;
 
 div_main.style.width = (baseCardSize + 10) * colCount + 130 + "px";
 div_header.style.width = (baseCardSize + 10) * colCount + 130 + "px";
@@ -44,29 +50,36 @@ img.src = "img_cards/cardback01_100x160.jpg";
 static_card.appendChild(img);
 div_main.appendChild(static_card);
 
-//cardRotateing = false;
 let baseX = static_card.offsetLeft; 
 let baseY = static_card.offsetTop;
 
 initImages(); // képek betöltése
-initCoords();
+initCoords(); // koordinátatömb init
 initCards();  // kártyát divjeinek létrehozása
-giveNewCoordsToCards();
-moveCardsToNewCoords();
+giveNewCoordsToCards(); // random kap koordinátákat minden kártya a koordináta tömbből
+cardsPositioning = true;
 
-function initImages() {
+function initImages() { // képek betöltése tömbbe
     for (let ci = 0; ci < 12; ci++) {
         img_num1 = (ci * 2);
         img_num2 = (ci * 2) + 1;
         images[img_num1] = document.createElement("img");
-        images[img_num1].src = "img_cards/card" + (ci + 1) + ".jpg";        
+        images[img_num1].src = "img_cards/card" + (ci + 1) + ".jpg"; 
+        images[img_num1].classList = "img_card";
+
         images[img_num1].style.height = baseCardSize * 1.6 +  "px";
+        //images[img_num1].style.height = "100%";
+
       //  images[img_num1].style.visibility = "hidden";
         images[img_num1].picID = ci + 1;
 
         images[img_num2] = document.createElement("img");
-        images[img_num2].src = "img_cards/card" + (ci + 1) + ".jpg";        
+        images[img_num2].src = "img_cards/card" + (ci + 1) + ".jpg";
+        images[img_num2].classList = "img_card";
+
         images[img_num2].style.height = baseCardSize * 1.6 +  "px";
+        //images[img_num2].style.height = "100%";        
+
       //  images[img_num2].style.visibility = "hidden";
         images[img_num2].picID = ci + 1;
     }
@@ -74,19 +87,27 @@ function initImages() {
     for (let ci = 0; ci < 24; ci++) {
         images_back[ci] = document.createElement("img");
         images_back[ci].src = "img_cards/cardback01_100x160.jpg";
+        images_back[ci].classList = "img_card";
+
         images_back[ci].style.height = baseCardSize * 1.6 +  "px";
+        //images_back[ci].style.height = '100%';
+
         images_back[ci].style.position ="absolute";
         //images_back[ci].style.visibility = "hidden";
     }
 }
 
-function initCoords() {
+function initCoords() { // koordinátatömb init        
     for (let y = 0; y < rowCount; y++)        
-        for (let x = 0; x < colCount; x++)                
-            coords.push({ X : x * (baseCardSize + 10), Y: y * ((baseCardSize * 1.6) + 10)});    
+        for (let x = 0; x < colCount; x++) {
+            coords.push({ X : x * (baseCardSize + 10), Y: y * ((baseCardSize * 1.6) + 10), 
+                          moveX: (baseX - (x * (baseCardSize + 10))) / 50,
+                          moveY: (baseY - (y * ((baseCardSize * 1.6) + 10))) / 50});
+
+        }
 }
 
-function giveNewCoordsToCards() {
+function giveNewCoordsToCards() { // random kap koordinátákat minden kártya a koordináta tömbből
     for (let ci = 0; ci < (rowCount * colCount); ci++) {
         cards[ci].coordNum = 0;
         cards[ci].style.display = "flex";
@@ -104,28 +125,39 @@ function giveNewCoordsToCards() {
     }
 }
 
-function moveCardsToNewCoords() {
-    for (let ci = 0; ci < (rowCount * colCount); ci++) {
+function tMoveCardsToNewCoords() { // átmozgatja a kártyákat az új koordinátákra
+    if (!cardsPositioning || cardsDocking) return;
+
+    giveNewCoordsToCards();
+    cardsPositioning = false;
+    cardDockedCount = 0;
+
+    for (let ci = 0; ci < (rowCount * colCount); ci++) {        
         cards[ci].style.left = coords[cards[ci].coordNum].X + "px";
         cards[ci].style.top  = coords[cards[ci].coordNum].Y + "px";
+        cards[ci].docked = false;
     }
+
 }
 
-function newGameClick() {
-    giveNewCoordsToCards();
-    moveCardsToNewCoords();    
+function newGameClick() { // új játék gombra klikkelés
+    if (cardDockedCount != rowCount * colCount) setAllDockingON();
+        
+    cardsPositioning = true;
+    flippedOne = -1; flippedTwo = -1;
 }
 
 function onMouseHover() { // égér a kártyalap felett
+    return;
     let h = this.style.height
-    p.innerHTML = "cardnum: " + this.cardNum + "<br>" + 
+    /*p.innerHTML = "cardnum: " + this.cardNum + "<br>" + 
                   "picnum: " + this.picNum + "<br>" +
                   this.style.left + "<br>" +
                   this.style.top + "<br>" + 
                   this.style.height + "<br>" +
                   this.offsetLeft + "<br>" +
                   this.offsetTop + "<br>" +
-                  "picID: " + this.picID;
+                  "picID: " + this.picID;*/
                
     this.style.width = baseCardSize + 14 + "px";
     this.style.height = baseCardSize * 1.6 + 14 + "px";
@@ -138,7 +170,8 @@ function onMouseHover() { // égér a kártyalap felett
     images_back[this.picNum].style.height = baseCardSize * 1.6 + 18 + "px";
 }
 
-function onMouseOutOfCard() {
+function onMouseOutOfCard() { // égér elmozgatása a kártyalapról
+    return;
     this.style.width = baseCardSize + "px";
     this.style.height = baseCardSize * 1.6 + "px";
     this.style.left = this.offsetLeft + 7 + "px";
@@ -150,14 +183,15 @@ function onMouseOutOfCard() {
     images_back[this.picNum].style.height = baseCardSize * 1.6 + "px";
 }
 
-function initOneCard(num, picnum, x, y) { // egy kártya inicializálása (num: kártyasorszám; picnum: kép sorszám)
+function initOneCard(num, picnum, x, y) { // egy kártya inicializálása
     cards[num] = document.createElement("div");
-    cards[num].style.height = baseCardSize * 1.6;   
-    cards[num].cardNum = num;
-    cards[num].picNum = picnum;
-    cards[num].coordNum = 0;
+    cards[num].style.height = baseCardSize * 1.6;
+    cards[num].cardNum = num;   // kártyasorszám
+    cards[num].picNum = picnum; // kép sorszám
+    cards[num].coordNum = 0;    // koordinátatömb sorszáma (újrakeveréskor random kap másikat)
+    cards[num].docked = false;  // a kártya vissza van -e rakva a pakliba
     cards[num].picID = images[picnum].picID;
-    cards[num].classList  = "div_onecard";            
+    cards[num].classList  = "div_onecard";
     cards[num].style.left = x + "px";
     cards[num].style.top  = y + "px";
 
@@ -170,13 +204,8 @@ function initOneCard(num, picnum, x, y) { // egy kártya inicializálása (num: 
     div_main.appendChild(cards[num]);    
 }
 
-function flipCard(num) { // num értkben megadott sorszámú kártya átfordítása
-    images_back[num].style.visibility = "hidden";
-    images[num].style.visibility = "visible";
-}
-
 function onCardClick() { // klikk a kártyán
-    if (cardRotate || cardMoveToBase) return; // if a card rotating animation is not finished, return
+    if (cardFlip || cardMoveToBase || cardsDocking) return; // if a card rotating animation is not finished, return
 
     if (this.cardNum == flippedOne || this.cardNum == flippedTwo) { // már át van fordítva / already flipped
         if (flippedOne != -1 && flippedTwo != -1) flipBackSelectedCards(); // már két kártya át van fordítva
@@ -187,7 +216,7 @@ function onCardClick() { // klikk a kártyán
         return;
     } 
 
-    flipCard(this.cardNum); // a kártya átfordítrása
+    //flipCard(this.cardNum, true); // a kártya átfordítrása
 
     if (flippedOne == -1) 
         flippedOne = this.cardNum
@@ -195,9 +224,16 @@ function onCardClick() { // klikk a kártyán
         flippedTwo = this.cardNum;
         checkIfSamePic(); // ha már két kártya van átfordítva, megvizsgálja, hogy azonosak-e
     }    
+
+    cardFlipToSeePic = true;
+    cardFlip = true;
 }
 
-function flipBackSelectedCards() { // visszafordítja az átfordított kértyákat
+function flipBackSelectedCards() { // visszafordítja az átfordított kártyákat
+    cardFlipToSeePic = false;
+    cardFlip = true;    
+    return;
+
     images_back[flippedOne].style.visibility = "visible";
     images[flippedOne].style.visibility = "hidden";
     images_back[flippedTwo].style.visibility = "visible";
@@ -208,73 +244,129 @@ function flipBackSelectedCards() { // visszafordítja az átfordított kértyák
 
 function checkIfSamePic() { // ha már két kártya van átfordítva, megvizsgálja, hogy azonosak-e
     if (cards[flippedOne].picID != cards[flippedTwo].picID) return;
-
-    distX1 = (baseX - cards[flippedOne].offsetLeft) / 100;
-    distY1 = (baseY - cards[flippedOne].offsetTop) / 100;
-    distX2 = (baseX - cards[flippedTwo].offsetLeft) / 100;
-    distY2 = (baseY - cards[flippedTwo].offsetTop) / 100;
-
     cardMoveToBase = true;
 }
 
+function moveCardOneStepIsDocked(cardNum) {
+    cards[cardNum].style.left = cards[cardNum].offsetLeft + coords[cards[cardNum].coordNum].moveX + "px";
+    cards[cardNum].style.top  = cards[cardNum].offsetTop  + coords[cards[cardNum].coordNum].moveY + "px";
+
+    if (baseX - cards[cardNum].offsetLeft < 5) {
+        cards[cardNum].style.left = baseX + "px";
+        cards[cardNum].style.top  = baseY + "px";
+        cards[cardNum.docked] = true;
+
+        cards[cardNum].style.display = "none";
+        cardDockedCount ++;
+        return true;
+    }
+
+    return false;
+}
+
+function setAllDockingON() {
+    cardsDocking = true;
+}
+
+function tAllCardsMoveToBase() {
+    if (!cardsDocking || cardDockedCount == (rowCount * colCount)) return;
+    
+    for (let cNum = 0; cNum < rowCount * colCount; cNum++) {
+        if (cards[cNum].docked) continue;
+    
+       // if (moveCardOneStepIsDocked(cNum)) cardsDocking = false;
+        moveCardOneStepIsDocked(cNum);
+        if (cardDockedCount == rowCount * colCount) 
+            cardsDocking = false;
+    }        
+}
+
 function tCardMoveToBase() {  // timer: a párosított kártyák visszatérnek a pakliba
-    if (!cardMoveToBase) return;
-
-    cards[flippedOne].style.left = cards[flippedOne].offsetLeft + distX1 + "px";
-    cards[flippedOne].style.top  = cards[flippedOne].offsetTop  + distY1 + "px";
-
-    cards[flippedTwo].style.left = cards[flippedTwo].offsetLeft + distX2 + "px";
-    cards[flippedTwo].style.top  = cards[flippedTwo].offsetTop  + distY2 + "px";
-
-    if (baseX - cards[flippedOne].offsetLeft < 5) {
-        cards[flippedOne].style.left = baseX + "px";
-        cards[flippedOne].style.top  = baseY + "px";
+    if (!cardMoveToBase || cardFlip) return;
+    
+    if (moveCardOneStepIsDocked(flippedOne)) {
         cards[flippedTwo].style.left = baseX + "px";
         cards[flippedTwo].style.top  = baseY + "px";
-
-        cards[flippedOne].style.display = "none";
+        cards[flippedTwo.docked] = true;
         cards[flippedTwo].style.display = "none";
+        cardDockedCount ++;
+
         flippedOne = -1; flippedTwo = -1;
-        cardMoveToBase = false;        
+        cardMoveToBase = false;
     }
+    else
+        moveCardOneStepIsDocked(flippedTwo);  
 }
 
 function initCards() {    
     for (let y = 0; y < rowCount; y++)        
         for (let x = 0; x < colCount; x++)                
-            initOneCard(y * colCount + x, // card number
-                        y * colCount + x, // picture number
-                       (x * (baseCardSize + 10)) + 0,   // x
+            initOneCard(y * colCount + x,                 // card number
+                        y * colCount + x,                 // picture number
+                       (x * (baseCardSize + 10)) + 0,     // x
                         y * ((baseCardSize * 1.6) + 10)); // y
 }
 
-function tMain() {
-    return;
+function flipCard(num, toSeePic) { // num értékben megadott sorszámú kártya átfordítása
+    if (toSeePic) {
+        images_back[num].style.visibility = "hidden";
+        images[num].style.visibility = "visible";    
+    }
+    else {
+        images[num].style.visibility = "hidden";    
+        images_back[num].style.visibility = "visible";
+    }
+}
+
+function changeSizeToActSize(cNum) {
+    cards[cNum].style.width = actCardSize + "px";
+    images_back[cNum].style.width = actCardSize + "px";
+    images[cNum].style.width = actCardSize + "px";
+    cards[cNum].style.marginLeft = (baseCardSize - actCardSize) / 2 + "px";
+}
+
+function tCardFlip() {
+    if (!cardFlip) return; // Csak akkor van tennivaló, ha éppen átfordul egy kártya
 
     if (!cardGrow) {
         actCardSize -= growNumber;
         if (actCardSize <= 0) {
             actCardSize = 0;
             cardGrow = true;
-            if (cardFront)
-                img.src = "img/backg02.jpg";
-            else
-                img.src = "img_cards/card001.jpg";
-            cardFront = !cardFront;            
+            if (cardFlipToSeePic) {
+                if (flippedTwo != -1) flipCard(flippedTwo, true)
+                else                  flipCard(flippedOne, true)
+            }
+            else {
+                flipCard(flippedOne, false);
+                flipCard(flippedTwo, false);
+            }
         }
     }
     else {
         actCardSize += growNumber;
         if (actCardSize >= baseCardSize) {
             actCardSize = baseCardSize;
-            cardGrow = true;
-            cardRotateing = false;            
+            if (!cardFlipToSeePic) {
+                flippedOne = -1; flippedTwo = -1;
+            }
+            cardGrow = false;            
+            cardFlip = false;
         }        
     }
 
-    card.style.width = actCardSize + "px";
+    if (flippedTwo != -1) {
+        changeSizeToActSize(flippedTwo);
+        if (!cardFlipToSeePic)
+            changeSizeToActSize(flippedOne);
+    }
+    else {
+        changeSizeToActSize(flippedOne);
+    }
+
+    /*  card.style.width = actCardSize + "px";
     img.style.width = actCardSize + "px";
-    card.style.marginLeft = (100 - actCardSize) / 2 + "px";
+    card.style.marginLeft = (100 - actCardSize) / 2 + "px";*/
 }
 
 function mouseXY(e) {
@@ -283,7 +375,7 @@ function mouseXY(e) {
 }
 
 function cardClick() {
-    if (cardRotateing) return;
+    /*if (cardRotateing) return;
     cardRotateing = true;
-    cardGrow = false;
+    cardGrow = false;*/
 }
