1a0bd06e841ded6e8dd91228fd2284f210a5734c
JakirteJak <67140731+JakirteJak@users.noreply.github.com>
2024-02-28 12:55:54 +0100

    new_vbf_list_view

--- JS/vbf_questions.js
+++ JS/vbf_questions.js
@@ -23,12 +23,39 @@ function MakeTableFromDatabase(){
     for (let ci = 0; ci < question3.data.length; ci++){
         let tr = document.createElement("tr");
         let td = document.createElement("td");
+
         td.innerHTML += '<p class = "table_questions textbold">' + question3.data[ci].q + "</p>";
+
+        tr.addEventListener('mouseover', event => {
+            pLine = event.currentTarget.getElementsByClassName('gA')[0];
+            if (pLine){
+                pLine.classList.add("goodAnswer");
+                pLine.classList.add("mouseOverModifier");
+                pLine.classList.remove("gA");
+            } 
+        });
+
+        tr.addEventListener('mouseout', event => {            
+            const trRect = event.currentTarget.getBoundingClientRect();
+            if (event.clientX >= trRect.left && event.clientX <= trRect.right &&
+                event.clientY >= trRect.top && event.clientY <= trRect.bottom) {
+                return;
+            }
+
+            pLine = event.currentTarget.getElementsByClassName('mouseOverModifier')[0];
+          
+            if (pLine){
+                pLine.classList.remove("goodAnswer");
+                pLine.classList.remove("mouseOverModifier");
+                pLine.classList.add("gA");
+            }
+        })
+        
         for (let cj = 0; cj < question3.data[ci].a.length; cj++){
             if (question3.data[ci].gA == cj + 1)
-                td.innerHTML += '<p style = "margin-left: 40px; color:green" class = "table_questions">' + question3.data[ci].a[cj] + "</p>";
+                td.innerHTML += '<p style = "margin-left: 40px;" class = "table_questions gA">' + question3.data[ci].a[cj] + "</p>";
             else
-                td.innerHTML += '<p style = "margin-left: 40px; color:black" class = "table_questions">' + question3.data[ci].a[cj] + "</p>";
+                td.innerHTML += '<p style = "margin-left: 40px;" class = "table_questions">' + question3.data[ci].a[cj] + "</p>";
         }
             
         td.style.paddingLeft  = "5px";
